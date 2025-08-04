0c8dffc7dfd2a3a76abffa791f287c83403568a4
AngelTah178 <148654569+AngelTah178@users.noreply.github.com>
2025-08-04 11:18:49 -0500

    Update create.php

--- HISTORIAS_CLINICAS/create.php
+++ HISTORIAS_CLINICAS/create.php
@@ -34,6 +34,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $ocupacion = $_POST['ocupacion'];
         $estado_civil = $_POST['estado_civil'];
 
+        //SELECIONAR DE PACIENTES EL ID DEL PACIENTE Y PASARLE LOS DATOS A CONSULTAR, NOMBRE, APELLIDO Y APELLIDO M
+        $sql_verificar = "SELECT id_paciente FROM pacientes 
+                  WHERE nombre_paciente = ? AND apellido_p = ? AND apellido_m = ?";
+//PREPARA UN STMT PARA VERIFICAR SI ES IGUAL AL SQL DE CONSULTA
+$verificar_stmt = $conn->prepare($sql_verificar);
+//PASA LOS MARCADORES DE POSICIÓN ASIGNADOS A SU RESPECTIVA VARIABLE, EN ESTE CASO SSS STRING
+$verificar_stmt->bind_param("sss", $nombre_paciente, $apellido_p, $apellido_m);
+//ESTO ES DE LOS PRIMEROS STMT QUE USAS EN CREATE PARA EJECUTAR
+$verificar_stmt->execute();
+$verificar_stmt->store_result();
+
+
+if ($verificar_stmt->num_rows > 0) {
+    // VERIFICAR SI YA EXISTEEE
+    $verificar_stmt->close();
+    $conn->rollback();
+    //SCRIPT PARA ALERTAR QUE YA EXISTE
+    echo "
+    <script>alert('El paciente ya está registrado.'); window.location.href = 'registroPaciente.php';</script>";
+    exit();
+}
+//CIERRA STATEMENT
+$verificar_stmt->close();
         // crear paciente
         $sql_paciente = "INSERT INTO pacientes (nombre_paciente, apellido_p, apellido_m, fecha_nacimiento, sexo, telefono, ocupacion, estado_civil)
                          VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
